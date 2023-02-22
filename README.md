908e6e8157a32a46a5f02a8babf6dbf7b98bc34d
hzuellig <zuellig@internauta.ch>
2023-02-22 12:08:17 +0100

    Project Chiada Rastelli

--- README.md
+++ README.md
@@ -1,14 +1,6 @@
+# Cross device interaction | ripple effect animation
 
-![di logo](https://github.com/digitalideation/comppx_h2001/blob/master/docs/assets/images/di-logo-small.jpg?raw=true "di logo")
+Hochschule Luzern | Digital Ideation | Studio Web & Mobile 2
 
-
-# Studio UX 2 and Web&Mobile Design & Engineering 2 - Spring 2023
-
-This repository includes resources & course notes for students attending Studio UX 2 and Web&Mobile Design & Engineering 2.<br/>
-Instructors: Angie Born, Guillaume Massol, Hanna Züllig, Maria Husmann<br/>
-Term: Spring 2023, HSLU<br/>
-
-
-
-[![MIT License](https://img.shields.io/badge/license-MIT-blue.svg)](http://opensource.org/licenses/MIT)
-![Mastodon Follow](https://img.shields.io/mastodon/follow/109315060138063198?domain=https%3A%2F%2Fswiss.social&style=social)
+Minimalist ripple effect animation that interacts with different devices on touch of the screen.
+Made with the p5.js library and websocket.
---
+++ public/sketch-wave.js
@@ -0,0 +1,108 @@
+var socket;
+var outDiam = 0;
+let circleClicked = 0;
+let clickPosX, clickPosY;
+let offsetbeginX;
+var sound;
+let opacityCol;
+let lineWeight;
+let waves = [];
+let num_waves; //no waves on the canvas at first
+let gradientBlue;
+let gradientGreen;
+
+let settings = {};
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    settings.w = width;
+    sound = loadSound("water effect echo.mp3", loaded);
+
+    socket = io.connect("http://localhost:3000/");
+
+    socket.on("click", newCircle);
+
+    socket.emit("get", settings);
+    socket.on("get", getSettings);
+
+    background(0);
+}
+
+function loaded() {}
+
+function getSettings(data) {
+    settings = data;
+    console.log(settings);
+}
+
+
+function newCircle(data) {
+    clickPosX = data.x;
+    clickPosY = data.y;
+    circleClicked = 1;
+
+    outDiam = 0;
+
+    let mywave = new Wave(data.x, data.y);
+    waves.push(mywave);
+}
+
+function touchStarted() {
+
+    sound.play();
+
+    var data = {
+        x: mouseX + settings.offsetbeginX,
+        y: mouseY
+    }
+    socket.emit("click", data);
+}
+
+function draw() {
+
+    gradientBlue = map(clickPosY, 120, windowHeight, 120, 30);
+    gradientGreen = map(clickPosY, 80, windowHeight, 80, 0);
+
+    background(10, gradientGreen, gradientBlue, 100);
+
+    let volumeSound = map(mouseX, 0.1, width, 0.1, 1);
+    sound.amp(volumeSound);
+
+    let speedSound = map(mouseY, 2, height, 2, 0.5);
+    sound.rate(speedSound);
+
+    for (const wav of waves) wav.display();
+
+}
+
+class Wave {
+    constructor(clickPosX, clickPosY) {
+        this.x = clickPosX;
+        this.y = clickPosY;
+        this.outDiam = 0;
+    }
+
+    display() {
+
+        if (circleClicked) {
+
+            push();
+            translate(-settings.offsetbeginX, 0);
+
+            for (let count = 0; count < 5; count++) {
+                let diam = this.outDiam - 80 * count;
+                if (diam > 0) {
+                    noFill();
+                    opacityCol = map(diam, 0, width * 2, 200, 0);
+                    lineWeight = map(diam, 0, width * 2, 10, 0)
+                    stroke(255, 255, 255, opacityCol);
+                    strokeWeight(lineWeight);
+                    ellipse(this.x, this.y, diam);
+                }
+            }
+            this.outDiam = this.outDiam + 10;
+            pop();
+        }
+    }
+}
